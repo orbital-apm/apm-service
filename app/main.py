b7eba9ff6c436c4feb8569b2f79c7efda49d2547
Phone Mon <e1121278z2u.nus.edu>
2024-05-20 14:09:06 +0800

    Test commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import *
 from app.api.v1.router import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+
+# User authentication endpoint
+
+
+
+
+# Tutorials and Guides endpoint
+
+class Tutorial(BaseModel):
+    id: int
+    title: str
+    content: str
+    category: str
+    created: str
+    updated: Optional[str] = None
+
+class Guide(BaseModel):
+    id: int
+    title: str
+    content: str
+    category: str
+    created: str
+    updated: Optional[str] = None
+
