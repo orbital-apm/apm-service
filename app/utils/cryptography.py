b0681a8aa8e555c0a505c92b4e866936167a04f1
alex <alex.liswandy@hotmail.com>
2024-06-09 22:51:58 +0800

    Added tests for utils.

--- app/utils/cryptography.py
+++ app/utils/cryptography.py
@@ -1,9 +1,9 @@
 import bcrypt
 
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
-
-
 def hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
