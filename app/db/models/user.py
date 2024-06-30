2e708279aa9b10444bb9e787d92eadc6f72aa894
Phone Mon <monluphone@gmail.com>
2024-06-30 17:32:03 +0800

    Flake8 fixes

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -5,9 +5,9 @@ from app.db.database import Base
 
 # Registered users table
 
+
 class UserEntity(Base):  # type: ignore[misc]
     __tablename__ = "users"
-    
     id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username: Mapped[str] = mapped_column(unique=True, nullable=True)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
--- app/main.py
+++ app/main.py
@@ -25,4 +25,3 @@ app.include_router(build_router.router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
-
